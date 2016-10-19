f270c63057858034937aa92049070375d6b7e8a7
Fernando Araujo <fmaraujo@gmail.com>
2016-10-19 14:30:57 -0200

    ajustes no texto do local de pagamento do Itaú para adequação no layout e ajuste no layout da parte de cima para boletos A4

--- src/OpenBoleto/Banco/Itau.php
+++ src/OpenBoleto/Banco/Itau.php
@@ -56,7 +56,7 @@ class Itau extends BoletoAbstract
      * Linha de local de pagamento
      * @var string
      */
-    protected $localPagamento = 'Até o vencimento, pague preferencialmente no Itaú. Após o vencimento pague somente no Itaú';
+//    protected $localPagamento = 'Até o vencimento, pague preferencialmente no Itaú. Após o vencimento pague somente no Itaú';
 
     /**
      * Define as carteiras disponíveis para este banco
