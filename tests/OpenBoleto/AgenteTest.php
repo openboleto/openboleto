13b07eb6a661f10fb7521170921b1ca5e0db9c0a
Rogerio Muniz <rogerio@quilhasoft.com.br>
2018-01-19 21:45:14 -0200

    Update AgenteTest.php

--- tests/OpenBoleto/AgenteTest.php
+++ tests/OpenBoleto/AgenteTest.php
@@ -2,9 +2,8 @@
 
 namespace Tests\OpenBoleto;
 use OpenBoleto\Agente;
-class_alias('\PHPUnit_Framework_TestCase', 'PHPUnit\Framework\TestCase');
 
-class AgenteTest extends \PHPUnit_Framework_TestCase
+class AgenteTest extends PHPUnit\Framework\TestCase
 {
     public function testInstantiationWithoutArgumentsShouldWork()
     {
