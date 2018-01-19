ac653ba5dc252fa71d34b8bb93d603dda8eb5f6b
Rogerio Muniz <rogerio@quilhasoft.com.br>
2018-01-19 21:58:40 -0200

    Update ItauTest.php

--- tests/OpenBoleto/Banco/ItauTest.php
+++ tests/OpenBoleto/Banco/ItauTest.php
@@ -3,7 +3,14 @@
 namespace Tests\OpenBoleto\Banco;
 use OpenBoleto\Banco\Itau;
 
-class ItauTest extends \PHPUnit_Framework_TestCase
+// Alias the PHPUnit 6.0 ancestor if available, else fall back to legacy ancestor
+if (class_exists('\PHPUnit\Framework\TestCase', true)) {
+  class KernelTestCaseAncestor extends \PHPUnit\Framework\TestCase {}
+} else {
+  class KernelTestCaseAncestor extends \PHPUnit_Framework_TestCase {}
+}
+
+class ItauTest extends KernelTestCaseAncestor
 {
     public function testInstantiateWithoutArgumentsShouldWork()
     {
