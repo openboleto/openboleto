35ca43d7f7d787a0f0fcfa68c87cbf33db3b572f
Rogerio Muniz <rogerio@quilhasoft.com.br>
2018-01-19 21:55:26 -0200

    Update BradescoTest.php

--- tests/OpenBoleto/Banco/BradescoTest.php
+++ tests/OpenBoleto/Banco/BradescoTest.php
@@ -3,7 +3,14 @@
 namespace Tests\OpenBoleto\Banco;
 use OpenBoleto\Banco\Bradesco;
 
-class BradescoTest extends \PHPUnit_Framework_TestCase
+// Alias the PHPUnit 6.0 ancestor if available, else fall back to legacy ancestor
+if (class_exists('\PHPUnit\Framework\TestCase', true)) {
+  class KernelTestCaseAncestor extends \PHPUnit\Framework\TestCase {}
+} else {
+  class KernelTestCaseAncestor extends \PHPUnit_Framework_TestCase {}
+}
+
+class BradescoTest extends KernelTestCaseAncestor
 {
     public function testInstantiateWithoutArgumentsShouldWork()
     {
