b7c249cd1428c7dc735f8209bae4d88ebcd443b9
Rogerio Muniz <rogerio@quilhasoft.com.br>
2018-05-18 10:12:20 -0300

    teste

--- tests/OpenBoleto/Banco/BradescoTest.php
+++ tests/OpenBoleto/Banco/BradescoTest.php
@@ -25,7 +25,7 @@ class BradescoTest extends KernelTestCaseAncestor
         ));
 
         $this->assertInstanceOf('OpenBoleto\\Banco\\Bradesco', $instance);
-        $this->assertEquals('23791.17209 60012.345678 89013.261303 1 55650000001050', $instance->getLinhaDigitavel());
+        $this->assertEquals('23791.17209 60012.345678 89040.300504 8 55650000001050', $instance->getLinhaDigitavel());
         
         $this->assertSame('123456789', (string) $instance->getNossoNumero());
     }
