eb6c209084954ce0ee9f8c65e339272039aa4abf
Fabrício Langer <fabriciolanger@live.com>
2024-01-02 19:46:57 -0400

    Pega o ano com base na data de emissao

--- src/OpenBoleto/Banco/Sicredi.php
+++ src/OpenBoleto/Banco/Sicredi.php
@@ -120,7 +120,8 @@ class Sicredi extends BoletoAbstract {
      * @return string
      */
     protected function gerarNossoNumero() {
-        $ano = date("y");
+        // $ano = date("y");
+        $ano = $this->dataDocumento->format('y');
 
         $numero = self::zeroFill($this->getAgencia(), 4) .
                 self::zeroFill($this->getPosto(), 2) .
